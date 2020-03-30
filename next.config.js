5be31f5fcbf97012c1dce67ea25ef2c39e8848ff
Ramnath Shreedhaar <42781349+unicornxoxo1919@users.noreply.github.com>
2020-03-30 10:00:46 +0400

    cache control

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 const withOffline = require("next-offline");
 
 const nextConfig = {
+  target: "serverless",
+  transformManifest: manifest => ["/"].concat(manifest), // add the homepage to the cache
+  // Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
+  // turn on the SW in dev mode so that we can actually test it
+  generateInDevMode: true,
   workboxOpts: {
     swDest: "static/service-worker.js",
     runtimeCaching: [
@@ -12,7 +17,7 @@ const nextConfig = {
           networkTimeoutSeconds: 15,
           expiration: {
             maxEntries: 150,
-            maxAgeSeconds: 30 // 1 month
+            maxAgeSeconds: 300 // 1 month
           },
           cacheableResponse: {
             statuses: [0, 200]

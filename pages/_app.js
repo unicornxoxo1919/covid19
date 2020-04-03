3bbd8617c63240ced4c551c245976bcf73592d1a
Ramnath Shreedhaar <42781349+unicornxoxo1919@users.noreply.github.com>
2020-04-03 15:23:23 +0400

    ga

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,25 @@
 import theme from "../theme";
 import Navbar from "../components/Navbar";
 import { ThemeProvider } from "theme-ui";
+import { initGA, logPageView } from "./googleAnalytics.js";
 
-const MyApp = ({ Component, pageProps }) => {
-  return (
-    <ThemeProvider theme={theme}>
-      <Navbar />
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
-};
+class MyApp extends React.Component {
+  componentDidMount() {
+    if (!window.GA_INITIALIZED) {
+      initGA();
+      window.GA_INITIALIZED = true;
+    }
+    logPageView();
+  }
+  render() {
+    this.props = { Component, pageProps };
+    return (
+      <ThemeProvider theme={theme}>
+        <Navbar />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    );
+  }
+}
 
 export default MyApp;
